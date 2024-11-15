0e65b885a269bec9db0129a64917c208e8e342ca
MeliodasCRM <Pabermb@gmail.com>
2024-11-15 20:19:54 +0100

    react-router-dom added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Details from './pages/Details'
 
 function App() {
-
   return (
-    <>
-    <h1>Hola mundo</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/details" element={<Details />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
 export default App
+
